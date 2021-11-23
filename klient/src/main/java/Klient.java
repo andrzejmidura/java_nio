dca06477e31430b02b733ab5db5cfdd7031ee521
andrzejmidura <andrzej.midura253@gmail.com>
2021-11-23 12:00:13 +0100

    refactoring and improvements

--- klient/src/main/java/Klient.java
+++ klient/src/main/java/Klient.java
@@ -3,20 +3,21 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class Klient {
     public void start() throws IOException {
+        Scanner scanner = new Scanner(System.in);
         SocketChannel socketChannel = SocketChannel.open();
         socketChannel.connect(new InetSocketAddress("localhost", 8090));
 
-        String newData = "New String to write to file..." + System.currentTimeMillis();
-
-        System.out.println("Data before sending to server: " + newData);
+        System.out.print("Type the message: ");
+        String dataToSend = scanner.nextLine();
 
         ByteBuffer in = ByteBuffer.allocate(48);
-        in.clear();
-        in.put(newData.getBytes());
 
+        in.clear();
+        in.put(dataToSend.getBytes());
         in.flip();
 
         while(in.hasRemaining()) {
@@ -33,7 +34,11 @@ public class Klient {
                 encoded[i] = out.get();
             }
         }
-        String received = new String(encoded, StandardCharsets.UTF_8);
+        String received = new String(encoded, StandardCharsets.UTF_8).trim();
         System.out.println("Data received from server: " + received);
+        System.out.print("Press enter to continue...");
+        scanner.nextLine();
+
+        socketChannel.close();
     }
 }
--- serwer/src/main/java/Serwer.java
+++ serwer/src/main/java/Serwer.java
@@ -3,44 +3,36 @@ import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 class Serwer {
-    // Z każdym klientem powiążemy kolejkę oczekujących na wysłanie buforów.
     private final Map<SocketChannel, Queue<ByteBuffer>> pendingData = new HashMap<>();
 
-    private ServerSocketChannel ssc;
+    private ServerSocketChannel serverSocketChannel;
     private Selector selector;
 
-    public void start() throws IOException, InterruptedException {
+    public void start() throws IOException {
         selector = Selector.open();
 
-        ssc = ServerSocketChannel.open();
-        ssc.bind(new InetSocketAddress(8090));
-        ssc.configureBlocking(false);
+        serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress(8090));
+        serverSocketChannel.configureBlocking(false);
 
-        // Chcemy być informowani o gotowości do akceptacji nowego połączenia.
-        ssc.register(selector, SelectionKey.OP_ACCEPT);
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
         while (true) {
-            TimeUnit.MILLISECONDS.sleep(10);
             selector.select();
             Set<SelectionKey> keys = selector.selectedKeys();
 
             for (Iterator<SelectionKey> it = keys.iterator(); it.hasNext(); ) {
 
                 SelectionKey sk = it.next();
-                // Należy pamiętać o usuwaniu przetworzonych kluczy!
                 it.remove();
 
                 if (sk.isValid() && sk.isAcceptable()) {
-                    // Nowy klient czeka na akceptację.
                     handleAccept();
                 } else if (sk.isValid() && sk.isReadable()) {
-                    // Możemy wykonać nieblokującą operację READ na kliencie.
                     handleRead(sk);
                 } else if (sk.isValid() && sk.isWritable()) {
-                    // Możemy wykonać nieblokującą operację WRITE na kliencie.
                     handleWrite(sk);
                 }
             }
@@ -48,72 +40,51 @@ class Serwer {
     }
 
     private void handleAccept() throws IOException {
-        SocketChannel sc = ssc.accept();
+        SocketChannel socketChannel = serverSocketChannel.accept();
 
-        if (sc != null) {
-            sc.configureBlocking(false);
-            // Chcemy być informowani o możliwości wykonania nieblokującej
-            // operacji READ na kliencie.
-            sc.register(selector, SelectionKey.OP_READ);
+        if (socketChannel != null) {
+            socketChannel.configureBlocking(false);
+            socketChannel.register(selector, SelectionKey.OP_READ);
 
-            pendingData.put(sc, new LinkedList<ByteBuffer>());
+            pendingData.put(socketChannel, new LinkedList<ByteBuffer>());
         }
     }
 
     private void handleRead(SelectionKey sk) throws IOException {
-        // Pobieramy kanał powiązany z zadanym kluczem.
-        SocketChannel sc = (SocketChannel) sk.channel();
-        ByteBuffer bb = ByteBuffer.allocate(120);
+        SocketChannel socketChannel = (SocketChannel) sk.channel();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(120);
 
-        // Czytamy z kanału sc do bufora bb. Zmienna read zawiera
-        // liczbę przeczytanych znaków.
-        int read = sc.read(bb);
+        int read = socketChannel.read(byteBuffer);
         if (read == -1) {
-            // -1 -> EOF. Usuwamy klienta z mapy i zamykamy połączenie.
-            pendingData.remove(sc);
-            sc.close();
+            pendingData.remove(socketChannel);
+            socketChannel.close();
         } else if (read > 0) {
-            // Aktualizujemy pozycję i limit w buforze, a także podmieniamy jego
-            // zawartość na wersję UPPERCASE.
-            bb.flip();
-            for (int i = 0; i < bb.limit(); i++) {
-                bb.put(i, (byte) Character.toUpperCase((char) bb.get(i)));
+            byteBuffer.flip();
+            for (int i = 0; i < byteBuffer.limit(); i++) {
+                byteBuffer.put(i, (byte) Character.toUpperCase((char) byteBuffer.get(i)));
             }
-            // Przetworzony bufor zostaje dodany do kolejki klienta.
-            pendingData.get(sc).add(bb);
-            // Po odczytaniu danych chcemy wysłać przetworzone wejście z powrotem
-            // do klienta, więc od teraz interesuje nas, kiedy można wykonać na nim
-            // nieblokującą operację WRITE.
+            pendingData.get(socketChannel).add(byteBuffer);
             sk.interestOps(SelectionKey.OP_WRITE);
         }
     }
 
     private void handleWrite(SelectionKey sk) throws IOException {
-        SocketChannel sc = (SocketChannel) sk.channel();
-        Queue<ByteBuffer> queue = pendingData.get(sc);
+        SocketChannel socketChannel = (SocketChannel) sk.channel();
+        Queue<ByteBuffer> queue = pendingData.get(socketChannel);
 
         while (!queue.isEmpty()) {
-            ByteBuffer bb = queue.peek();
-            // Piszemy do kanału sc z bufora bb. Zmienna write zawiera 
-            // liczbę wysłanych znaków.
-            int write = sc.write(bb);
+            ByteBuffer byteBuffer = queue.peek();
+            int write = socketChannel.write(byteBuffer);
             if (write == -1) {
-                pendingData.remove(sc);
-                sc.close();
+                pendingData.remove(socketChannel);
+                socketChannel.close();
                 return;
-            } else if (bb.hasRemaining()) {
-                // Nie udało się wysłać całej zawartości bufora. Oznacza to, że w trakcie
-                // wykonywania operacji write przestało być możliwe nieblokujące
-                // wysyłanie danych. Opuszczamy metodę - reszta bufora zostanie wysłana
-                // przy następnej okazji.
+            } else if (byteBuffer.hasRemaining()) {
                 return;
             }
 
-            // Wysłaliśmy cały bufor. Usuwamy go z kolejki.
             queue.remove();
         }
-
-        // Wysłaliśmy odpowiedź. Wracamy do nasłuchiwania zapytań od klienta.
         sk.interestOps(SelectionKey.OP_READ);
     }
 }
